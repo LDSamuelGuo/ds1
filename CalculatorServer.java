b66cf502ff5de0dbcf63c0100f4ef5f590907391
LDSamuelGuo <105694995+LDSamuelGuo@users.noreply.github.com>
2024-08-04 16:23:37 +0930

    Update CalculatorServer.java

--- CalculatorServer.java
+++ CalculatorServer.java
@@ -2,20 +2,22 @@ import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class CalculatorServer {
-    public static void main(String args[]) {
-	try {
-		
-	    CalculatorImplementation obj = new CalculatorImplementation();
-	
-	    Calculator stub = (Calculator) UnicastRemoteObject.exportObject(obj, 0);
+public class CalculatorServer
+{
+    public static void main (String[] args)
+    {
+        try
+        {
+            CalculatorImplementation calculator = new CalculatorImplementation();
+            Calculator stub = (Calculator) UnicastRemoteObject.exportObject(calculator, 0);
 
-	    
-	    Registry registry = LocateRegistry.getRegistry();
-	    registry.rebind("Calculator", stub);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind("Calculator", stub);
 
-	    System.err.println("Server ready");
-        } catch (Exception e) {
+            System.err.println("Server ready");
+        }
+        catch (Exception e)
+        {
             System.err.println("Server exception: " + e.toString());
             e.printStackTrace();
         }
